61fd5febd0f9d649375d26b1ac87b3454344d9d0
Aleksander Morawski <olek.morawski@gmail.com>
2022-08-30 17:29:34 +0200

    random

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,24 +1,29 @@
 const { network } = require("hardhat")
-const {
-    DECIMALS,
-    INITIAL_ANSWER,
-    devChains,
-} = require("../helper-hardhat-config")
 
+const DECIMALS = "8"
+const INITIAL_PRICE = "200000000000" // 2000
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-
-    if (devChains.includes(network.name)) {
+    const chainId = network.config.chainId
+    // If we are on a local development network, we need to deploy mocks!
+    if (chainId == 31337) {
         log("Local network detected! Deploying mocks...")
         await deploy("MockV3Aggregator", {
             contract: "MockV3Aggregator",
             from: deployer,
             log: true,
-            args: [DECIMALS, INITIAL_ANSWER],
+            args: [DECIMALS, INITIAL_PRICE],
         })
-        log("Mocks deployed")
-        log("====================================================")
+        log("Mocks Deployed!")
+        log("------------------------------------------------")
+        log(
+            "You are deploying to a local network, you'll need a local network running to interact"
+        )
+        log(
+            "Please run `npx hardhat console` to interact with the deployed smart contracts!"
+        )
+        log("------------------------------------------------")
     }
 }
 module.exports.tags = ["all", "mocks"]
--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,5 @@
-const { network } = require("hardhat")
-const { networkConfig, devChains } = require("../helper-hardhat-config")
+const { getNamedAccounts, deployments, network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
@@ -7,27 +7,30 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     let ethUsdPriceFeedAddress
-    if (devChains.includes(network.name)) {
+    if (chainId == 31337) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
-
-    const args = [ethUsdPriceFeedAddress]
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: args,
+        args: [ethUsdPriceFeedAddress],
         log: true,
+        // we need to wait if on a live network so we can verify properly
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+    log(`FundMe deployed at ${fundMe.address}`)
 
-    if (!devChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        await verify(fundMe.address, args)
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
     }
-
-    log("==================================================")
 }
+
 module.exports.tags = ["all", "fundme"]
